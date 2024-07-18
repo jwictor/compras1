4b6a55af2467e2143096c1146696a8bff6e0da5c
jwictor <joaowictoroliveira3301@gmail.com>
2024-07-18 20:44:05 -0300

    atualiza 1807

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,17 @@
 function Login() {
     return(
         <div className="container-login">
-            <h1>Login</h1>
+            <form className="form-login">
+                <h1>{`</Login`}</h1>
+                <label htmlFor="">Login</label>
+                <input type="text"  name="username"/>
+                <label htmlFor="">Senha</label>
+                <input type="password" name="password" />
+                <div className="buttons">
+                    <button className="button-confirmar">Confirmar</button>
+                    <button className="button-cancelar">Cancelar</button>
+                </div>
+            </form>
         </div>
     )
 }
